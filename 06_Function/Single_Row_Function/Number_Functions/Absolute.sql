9ea1ba6fac5a7082a4718b11ff28ea5e7e091d0d
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2026-05-15 15:43:39 +0530

    Numbers Functions

---
+++ 06_Function/Single_Row_Function/Number_Functions/Absolute.sql
@@ -0,0 +1,5 @@
+
+SELECT
+-10 as original_value,
+abs(-10) as absolute_value,
+abs(10) as absolute_value_positive;
---
+++ 06_Function/Single_Row_Function/Number_Functions/Round.sql
@@ -0,0 +1,7 @@
+
+
+SELECT 
+5.152 as orignal_value,
+round(5.152) as rounded_value_0,
+round(5.152 , 1) as rounded_value_1,
+round(5.152 , 2) as rounded_value_2;
