9af0b3c4d246ba03da9954eff946be79714dc029
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2026-06-04 16:08:05 +0530

    Cache Storage

---
+++ 08_Advanced_SQL_Techniques/Storage/Cache/Cache.sql
@@ -0,0 +1,3 @@
+
+-- * Cache storage is used to hold frequently accessed data in memory for faster retrieval.
+-- * It is stored in the database's memory, and it is used by the database to improve query performance.
